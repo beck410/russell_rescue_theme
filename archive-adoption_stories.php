e04be82a433a8dbdce951ade21893625b8e4a44d
Rebecca Cronin-Dixon <beck410@hotmail.com>
2014-11-06 03:24:52 +0000

    added custom post type: adoption_stories & styled its archive

---
+++ archive-adoption_stories.php
@@ -0,0 +1,37 @@
+<?php get_header() ?>
+<?php
+    $args = array( 'post_type' => 'adoption_stories', 'posts_per_page' => 10 );
+    $loop = new WP_Query( $args );
+
+    while ( $loop->have_posts() ) : $loop->the_post();
+        $little_excerpt = substr(get_the_excerpt(),0,400);
+        ?>
+        <div class="row">
+            <div class="col-md-10 col-md-push-1 blue-box">
+                <div class="row">
+                    <div class="col-md-8 col-xs-8 box-titles blue-box-title">
+                        <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                    </div> <!-- .col 8 blue-box-title -->
+                    <div class="col-md-4 col-xs-4 blue-box-stripes">
+                    </div> <!-- col 4 blue-box-stripes --> 
+                </div> <!-- row--> 
+                <div class="row">
+                <?php if(has_post_thumbnail() ) { ?>
+                    <div class="col-md-6 col-md-push-3">
+                        <img src = "<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID) ); ?>" />
+                    </div> <!-- col 12 -->
+                <?php
+                    }
+                ?>
+                </div> <!--row --> 
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3><strong><?php echo the_title() ?>'s Happily Furever After</strong></h3>
+                        <p><?php echo $little_excerpt; ?><span><a href="<?php the_permalink(); ?>"></span>Read More...<span></p>
+                    </div><!-- col 12 -->
+                </div> <!-- row -->
+            </div> <!-- col 10 blue-box -->
+        </div> <!-- row -->
+    <?php endwhile; ?>
+    
+<?php get_footer() ?>
--- archive-events.php
+++ archive-events.php
@@ -10,88 +10,42 @@
             $cost = get_post_meta( $post->ID, 'event_cost', true );
             $little_excerpt = substr(get_the_excerpt(),0,200);
             $id = $post->ID;
-            
-            if($id%2 == 0){
-                ?>
-                <div class="row">
-                    <div class="col-md-10 col-md-push-1 green-box">
-                        <div class="row">
-                            <div class="col-md-8 col-xs-8 box-titles green-box-title">
-                                <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
-                            </div> <!-- .col 8 green-box-title -->
-                            <div class="col-md-4 col-xs-4 green-box-stripes">
-                            </div> <!-- green-box-stripes --> 
-                        </div> <!-- row--> 
-                        <div class="row">
-                            <div class="col-md-4 col-xs-6">
-                                <?php
-                                if( has_post_thumbnail() ) { ?>
-                                    <img src= "<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>" />
-                                <?php
-                                }
-                                else {
-                                    echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/default_events.jpg" />';
-                                }
-                                ?>
-                            </div> <!-- col 4 -->
-                            <div class="col-md-8 col-xs-6">
-                                <p><strong>Where: </strong><span><?php echo $location ?></span></p>
-                                <p><strong>When: </strong><span><?php echo $date ?></span></p>
-                                <?php
-                                if( !empty( $cost ) ) {
-                                  echo '<p><strong>Cost:</strong> $<span>'.$cost.'</span></p>';  
-                                }
-                                ?>
-                                <p><strong>Description: </strong><?php echo $little_excerpt; ?><span>... </span><a href="<?php the_permalink(); ?>"></span>More Info<span></p>
-                            </div> <!-- col 8 -->  
-                        </div> <!-- row -->
-                    </div><!-- col 12 green-box -->
-                </div><!--row -->
-                <?php
-            }
-            
-            else{
-                ?>
-                <div class="row"> 
-                    <div class="col-md-10 col-md-push-1 orange-box">
-                         <div class="row">
-                            <div class="col-md-8 col-xs-8 box-titles orange-box-title">
-                                <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
-                            </div> <!-- .col 8 orange-box-title -->
-                            <div class="col-md-4 col-xs-4 orange-box-stripes">
-                            </div> <!-- orange-box-stripes --> 
-                        </div> <!-- row -->
-                             <div class="row">
-                                <div class="col-md-4 col-xs-6">
-                                <?php
-                                if( has_post_thumbnail() ) { ?>
-                                    <img src= "<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>" />
-                                <?php
-                                }
-                                else {
-                                    echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/default_events.jpg" />';
-                                }
-                                ?>
-                            </div> <!-- col 4 -->
-                            <div class="col-md-8 col-xs-6">
-                                <p><strong>Where: </strong><span><?php echo $location ?></span></p>
-                                <p><strong>When: </strong><span><?php echo $date ?></span></p>
-                                <?php
-                                if( !empty( $cost ) ) {
-                                  echo '<p><strong>Cost:</strong> $<span>'.$cost.'</span></p>';  
-                                }
-                                ?>
-                                <p><strong>Description: </strong><?php echo $little_excerpt; ?><span>... </span><a href="<?php the_permalink(); ?>"></span>More Info<span></p>
-                            </div> <!-- col 8 -->  
-                        </div> <!-- row -->
-                    </div> <!-- orange-box -->
-                </div><!-- row -->
-                <?php
-                
-            }
-            
-        endwhile;
-    ?>
 
+            ?>
+            <div class="row">
+                <div class="col-md-10 col-md-push-1 green-box">
+                    <div class="row">
+                        <div class="col-md-8 col-xs-8 box-titles green-box-title">
+                            <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                        </div> <!-- .col 8 green-box-title -->
+                        <div class="col-md-4 col-xs-4 green-box-stripes">
+                        </div> <!-- green-box-stripes --> 
+                    </div> <!-- row--> 
+                    <div class="row">
+                        <div class="col-md-4 col-xs-6">
+                            <?php
+                            if( has_post_thumbnail() ) { ?>
+                                <img src= "<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>" />
+                            <?php
+                            }
+                            else {
+                                echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/default_events.jpg" />';
+                            }
+                            ?>
+                        </div> <!-- col 4 -->
+                        <div class="col-md-8 col-xs-6">
+                            <p><strong>Where: </strong><span><?php echo $location ?></span></p>
+                            <p><strong>When: </strong><span><?php echo $date ?></span></p>
+                            <?php
+                            if( !empty( $cost ) ) {
+                              echo '<p><strong>Cost:</strong> $<span>'.$cost.'</span></p>';  
+                            }
+                            ?>
+                            <p><strong>Description: </strong><?php echo $little_excerpt; ?><span>... </span><a href="<?php the_permalink(); ?>"></span>More Info<span></p>
+                        </div> <!-- col 8 -->  
+                    </div> <!-- row -->
+                </div><!-- col 12 green-box -->
+            </div><!--row -->
+        <?php endwhile; ?>
 
 <?php get_footer() ?>
