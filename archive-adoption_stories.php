dee2a7a8dcfecf19a36c7668c87b62057efbb1db
Rebecca Cronin-Dixon <beck410@hotmail.com>
2015-01-28 02:58:23 +0000

    fixed archive_dog_stories and single_dog_stories structure and style

--- archive-adoption_stories.php
+++ archive-adoption_stories.php
@@ -6,32 +6,28 @@
     while ( $loop->have_posts() ) : $loop->the_post();
         $little_excerpt = substr(get_the_excerpt(),0,400);
         ?>
-        <div class="row">
-            <div class="col-md-10 col-md-push-1 blue-box">
-                <div class="row">
-                    <div class="col-md-8 col-xs-8 box-titles blue-box-title">
-                        <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
-                    </div> <!-- .col 8 blue-box-title -->
-                    <div class="col-md-4 col-xs-4 blue-box-stripes">
-                    </div> <!-- col 4 blue-box-stripes --> 
-                </div> <!-- row--> 
-                <div class="row">
+        
+        <div class="col-md-10 col-md-push-1 box">
+            <div class="box-title blue-box-title cf">
+                <div class="col-md-8 col-xs-8 blue-title-header">
+                    <h3><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                </div><!-- col-8 -->
+                <div class="col-md-4 col-xs-4"></div>
+            </div><!-- box-title -->
+            <div class="blue-box-content cf">
+                <div class="col-md-6 col-md-push-3 archive-image">
                 <?php if(has_post_thumbnail() ) { ?>
-                    <div class="col-md-6 col-md-push-3 archive-image">
-                        <img src = "<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID) ); ?>" />
-                    </div> <!-- col 12 -->
-                <?php
-                    }
-                ?>
-                </div> <!--row --> 
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3><strong><?php echo the_title() ?>'s Happily Furever After</strong></h3>
-                        <p><?php echo $little_excerpt; ?><span><a href="<?php the_permalink(); ?>"></span>Read More...<span></p>
-                    </div><!-- col 12 -->
-                </div> <!-- row -->
-            </div> <!-- col 10 blue-box -->
-        </div> <!-- row -->
+                    <img src = "<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID) ); ?>" />
+                <?php } else {
+                        echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/archive-dog-default.jpg" />';
+                    } ?>
+                </div>
+                <div class="col-md-12">
+                    <h3><strong><?php echo the_title() ?>'s Happily Furever After</strong></h3>
+                    <p><?php echo $little_excerpt; ?><span><a href="<?php the_permalink(); ?>"></span> Read More...<span></p>
+                </div><!-- col 12 -->
+            </div><!--blue-box-content -->
+        </div> <!-- box -->
     <?php endwhile; ?>
     
 <?php get_footer() ?>
