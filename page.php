d7db94c7346dc29cba4955659ce54207f04f3c03
Rebecca Cronin-Dixon <beck410@hotmail.com>
2015-02-03 00:05:38 +0000

    fixed volunteer page

--- page.php
+++ page.php
@@ -11,15 +11,18 @@
  */
 
 get_header(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+	<div class="col-md-12 box page_info"> 
+		<div class="box-title orange-box-title cf">
+			<div class="col-md-8 col-xs-8 orange-title-header">
+ 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>  
+            </div><!-- col-8 -->
+            <div class="col-md-4 col-xs-4"></div>
+		</div>
+		<div class="orange-box-content cf">
+			<?php the_content(); ?>
+		</div>
+	</div>
 
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-
-			<?php endwhile; // end of the loop. ?>
-
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	<?php endwhile; // end of the loop. 
+	get_footer(); ?>
